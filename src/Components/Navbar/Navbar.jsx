6b18759e1ea881e077718fd29f1b5dc72f6a072b
nidzhat-2000 <nidzhat.niyazov@gmail.com>
2023-01-19 02:21:22 +0400

    About fixed"

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -30,7 +30,7 @@ export default function MainNav() {
           </Navbar.Brand>
           <Nav bsPrefix="nav_buttons" className={styles.nav_buttons}>
             <Link
-              to="About_about__xPgdP"
+              to="About_about__1eeQa"
               spy={true}
               smooth={true}
               offset={-150}
--- src/Pages/Desserts/Desserts.jsx
+++ src/Pages/Desserts/Desserts.jsx
@@ -2,18 +2,23 @@ import React, { useEffect, useState } from 'react';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import useFetch from '../../utils/useFetch';
 import styles from './Desserts.module.css';
-
-// import Box from '@mui/material/Box';
+import Box from '@mui/material/Box';
 
 export default function Desserts() {
   const { data } = useFetch('../data/data.json');
   const header = data?.dessertsPage.header;
   const products = data?.dessertsPage.products;
-  
+
   return (
     <section className={styles.dessert}>
       <h2 className="header">{header}</h2>
-      <article className={styles.dessert_box}>
+      <Box
+        style={{
+          display: 'grid',
+          gap: '36px',
+          gridTemplateColumns: '1fr 1fr 1fr',
+        }}
+      >
         {products?.map((product, i) => {
           const { img, info } = product;
           return (
@@ -23,7 +28,7 @@ export default function Desserts() {
             </div>
           );
         })}
-      </article>
+      </Box>
     </section>
   );
 }
